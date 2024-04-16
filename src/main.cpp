2af4ffabd1ff4d138d82929aba01865a71c0a80a
hugo <blomdahl.hugo@protonmail.com>
2024-04-16 15:23:03 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include <Wifi.h>
+#include <HTTPclient.h>
+
+void initWiFi(String ssid, String password) {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  //25s to connect
+  for(int i=0; i<25; i++){
+    if(WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(1000);
+    }
+    else{
+      Serial.println(WiFi.localIP());
+      i = 1000; //break 
+    }
+  }
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Not connected!");
+  }
+  
+}
+
+void sendJson(String serverPath, String jsonData){
+  HTTPClient http;
+  
+  http.begin(serverPath.c_str());
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST(jsonData);
+  Serial.print(httpResponseCode);
+}
+
+void setup() {
+  Serial.begin(9600);
+  // Enable station mode (wifi client) and disconnect from previous
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
+  initWiFi("ssid", "password");
+}
+
+void loop() {
+  String serverName = "http://172.0.0.1:1880/";
+  String testData = "{\"temp\":\"23\",\"humidity\":\"19\"}";
+  sendJson(serverName, testData);
+  delay(10000);
+}
