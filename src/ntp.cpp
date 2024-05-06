bd1a8c9608f395365d26d8ae30b722309143f88d
EmilRid <emilridulfo@gmail.com>
2024-05-06 10:50:26 +0200

    add NTP pool specification

--- src/ntp.cpp
+++ src/ntp.cpp
@@ -2,18 +2,24 @@
 #include <WiFiUdp.h>
 #include <TimeLib.h>
 
+char* serverPool = "";
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
 void initNTP() {
   // Initialize a NTPClient to get time
   timeClient.begin();
+  
   // Set offset time in seconds to adjust for your timezone, for example:
   // GMT +1 = 3600
   // GMT +8 = 28800
   // GMT -1 = -3600
   // GMT 0 = 0
   timeClient.setTimeOffset(7200);
+
+  
+  // If the serverPool is defined, set timeClient pool to serverPool
+  if(serverPool != "") timeClient.setPoolServerName(serverPool);
 }
 
 String formatData(int value) {
